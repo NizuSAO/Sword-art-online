25e2313bc9cf49a36ad7bb0df04f5b254e6808fc
NizuSAO <nizusao@gmail.com>
2021-11-08 18:19:07 -0300

    mi primer commit

---
+++ formularioregistro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial scale=1.0">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link rel="stylesheet" href="sao.css">
+	<title>Sword art online</title>
+</head>
+<body>
+	<form method="post">
+	<h1 style="color: white">Sao Unital Ring Space</h1>
+	<section class="form-register">
+		<h3>Registro de jugador</h3>
+		<input class="controls" type="text" name="nombres" placeholder="ingresar su id de jugador">
+		<input class="controls" type="password" name="contraseña" placeholder="ingresar su contraseña">
+		<input class="controls" type="email" name="correo" placeholder="ingresar su email">
+		<input class="controls" type="date" name="fecha" placeholder="ingresar su fecha">
+		<p>Estoy de acuerdo con los <a href="">terminos y condiciones</a> </p>
+		<input class="botons" type="submit" name="Registrar">
+		<p><a href="#">ya tengo cuenta</a></p>
+	</section>
+	</form>
+
+	<?php
+	include("registrar.php") 
+	 ?>
+
+</body>
+</html>
---
+++ registrar.php
@@ -0,0 +1,15 @@
+<?php
+include("db.php");
+
+if (isset($_POST['Registrar'])) {
+	if (strlen($_POST['nombres']) >= 1 && strlen($_POST['contraseña']) >= 1 && strlen($_POST['correo']) >= 1) 
+		{
+			$name = trim($_POST['nombres']);
+			$contraseña = trim($_POST['contraseña']);
+			$email =  trim($_POST['correo']);
+			$fecha = trim($_POST['fecha']);
+			$consulta = "INSERT INTO users(`id_usuario`, `contraseña`, `email`, `fecha_registro`) VALUES ('$name','$contraseña','$email','$fecha')";
+			$resultado = mysqli_query($conn,$consulta);
+		}
+}
+?>
